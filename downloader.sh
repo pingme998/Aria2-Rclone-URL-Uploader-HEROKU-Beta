6a8642017cc896f3b1c2e6467c4b7eda8b0f0eab
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-08-25 14:20:28 +0530

    Update downloader.sh

--- downloader.sh
+++ downloader.sh
@@ -5,14 +5,15 @@ mkdir /tmp/$cloudname1
 touch /tmp/$cloudname1/progression.log
 log="/progression.log"
 
-match="SEED"
+killif="SEED"
+killelse="(OK):download completed"
 
 aria2c --dir=/home/$cloudname1 --input-file=/home/$cloudname1/urls.txt --max-concurrent-downloads=1 --connect-timeout=60 --max-connection-per-server=$threats1 --split=$threats1 --min-split-size=1M --human-readable=true --download-result=full --file-allocation=none > "$log" 2>&1 &
 while sleep 5
 do
 clear
 cat /progression.log | grep ETA |tail -1
-    if grep --quiet 'SEED\|(OK):download completed' "$log"
+    if grep --quiet "$killif\|$killelse" "$log"
     then
         pkill aria2c
         exit 0
