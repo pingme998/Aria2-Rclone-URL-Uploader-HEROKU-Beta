4516d9b479b518f64095abbf6a1193fb8625f292
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-08-27 14:16:48 +0530

    Update downloader1.sh

--- downloader1.sh
+++ downloader1.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+
+mkdir /home/1
+strace -p$(pgrep aria2c) -s9999 -e write -o /home/1/progx.log && while :; do sleep 1; cat /home/1/progx.log |sed 's/\[/\n/g' |sed 's/]/\n/g' |grep ETA |tail -1 && aria2c --dir=/home/$() --input-file=/urls.txt --enable-color=false --max-concurrent-downloads=1 --connect-timeout=60 --max-connection-per-server="{threats.value}" --split="{threats.value}" --min-split-size=1M --human-readable=true --download-result=full 
