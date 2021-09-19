54ddf90cf614ec23deab8fbaab41b37526140806
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-09-20 00:34:37 +0530

    Update downloader.sh

--- downloader.sh
+++ downloader.sh
@@ -1,23 +1,9 @@
-#!/bin/bash
-cloudname1=$(cat /cloudvale|sed "s/,/\n/g"|grep value |sed "s/value='//g" |sed "s/')//g")
-threats1=$(cat /threats|sed "s/,/\n/g"|grep value |sed "s/value='//g" |sed "s/')//g")
-mkdir /tmp/$cloudname1
-touch /tmp/$cloudname1/progression.log
-log="/progression.log"
+PRESET="ultrafast" # ultrafast, superfast, veryfast, faster, fast, medium, slow, slower, veryslow, placebo
+SOURCE="/storage/emulated/0/Download/cb.mp4"
+YOUTUBE_URL="rtmp://in.push.booyah.live/live"
+KEY="34884818?token=126a8a59661513ac25b2e0af24e2c046f3acaab8256074ee19cee030" # Your youtube key. (https://www.youtube.com/live_dashboard > encoder config > name/key)
 
-killif="SEED"
-killelse="(OK):download completed"
-
-aria2c --dir=/home/$cloudname1 --input-file=/home/$cloudname1/urls.txt --max-concurrent-downloads=1 --connect-timeout=60 --max-connection-per-server=$threats1 --split=$threats1 --min-split-size=1M --human-readable=true --download-result=full --file-allocation=none > "$log" 2>&1 &
-while sleep 5
-do
-clear
-cat /progression.log | grep ETA |tail -1
-    if grep --quiet "$killif\|$killelse" "$log"
-    then
-        pkill aria2c
-        exit 0
-    fi
-done
-
-while sleep 5; do clear; cat /progression.log | grep ETA |tail -1;     if grep --quiet 'SEED\|(OK):download completed' "$log";     then         pkill aria2c;         exit 0;     fi; done
+ffmpeg \
+    -re -i "$SOURCE" -vcodec libx264 -preset $PRESET -maxrate 3000k -b:v 2500k \
+    -bufsize 600k -pix_fmt yuv420p -g 60 -c:a aac -b:a 160k -ac 2 \
+    -ar 44100 -f flv -s 1280x720 "$YOUTUBE_URL/$KEY"
